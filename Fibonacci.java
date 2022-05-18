0366b195e60d046a6a6b820a533375bb869e5c94
Josh <98499717+KohetsuCoding@users.noreply.github.com>
2022-05-18 13:19:54 -0400

    Add files via upload

--- Fibonacci.java
+++ Fibonacci.java
@@ -5,6 +5,13 @@ public class Fibonacci {
         System.out.println("The " + fibNumber + "th term of the Fibonacci sequence is " + fibonacciRecursive(fibNumber));
     }//End Main
 
+    /**
+     *This method is a static recursive method that finds the nth term of the fibonacci sequence
+     *
+     * @author      Josh
+     * @param   n   a user-defined integer
+     * @return      The nth term of the Fibonacci sequence as an integer
+     */
     public static int fibonacciRecursive(int n) {
         if(n <= 1) {
             return n;
